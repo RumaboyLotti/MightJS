908e8dac2a75534b1b08e1341573e1921d6fca75
Ladi Bada <46547545+RumaboyLotti@users.noreply.github.com>
2023-09-14 00:12:16 -0500

    js version works and faster

---
+++ index.js
@@ -0,0 +1,24 @@
+// var mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//   host: '127.0.0.1',
+//   port: 3306,
+//   user: 'root',
+//   password: 'Xenophobia',
+//   database: 'Might-Auth',
+// });
+  
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("Error connecting to the database:", err);
+//     // Handle the error here or perform any necessary cleanup
+//     // For example, you might want to gracefully exit the application
+//     process.exit(1); // Exit the Node.js process with a non-zero status code
+//   }
+//   console.log("Connected!");
+// });
+
+function addUser(){
+    return document.getElementById('test').innerHTML = 'Completed';
+
+}
