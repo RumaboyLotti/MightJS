d4968976c96f00de0da688d71fe23f4794b70909
Ladi Bada <46547545+RumaboyLotti@users.noreply.github.com>
2023-09-18 15:28:11 -0500

    migrated changes to my express server

--- routes/app.js
+++ routes/app.js
@@ -1,35 +1,74 @@
-require("dotenv").config();
-
-const express = require("express");
-const session = require("express-session");
-const request = require("request-promise");
-const path = require("path");
-const router = express.Router();
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Client } = require('pg');
 
 const app = express();
+const port = 3000;
 
+// Set up a PostgreSQL client
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: 'Xylophobia',
+  database: 'Might-Auth',
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve your static files (e.g., HTML, CSS, client-side JS)
 app.use(express.static('public'));
-app.use(addUser);
-app.use("/", router);
 
-app.use(
-  session({
-    secret: "your_secret_here",
-    resave: false,
-    saveUninitialized: true,
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+function addUser() {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  const formData = new FormData(document.querySelector('form'));
+
+  fetch('/submitForm', {
+    method: 'POST',
+    body: formData,
   })
-);
-
-app.get("/", (req, res) => {
-  res.sendFile('index.html', { root: __dirname });
-});
-
-
-app.listen(3000, () => {
-  console.log("Server listening on http://localhost:3000");
-});
-
-function addUser(req, res) {
-  return (console.log("addUser"));
-
+    .then((response) => response.text())
+    .then((data) => {
+      console.log('Form submission response:', data);
+    })
+    .catch((error) => {
+      console.error('Error submitting form:', error);
+    });
 }
+
+app.post('/submitForm', (req, res) => {
+  const { username, firstName, lastName, email, password } = req.body;
+
+  client.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    const sql = `INSERT INTO users (UserName, FirstName, LastName, Email, UserPassword) 
+                 VALUES ($1, $2, $3, $4, $5)`;
+    const values = [username, firstName, lastName, email, password];
+
+    client.query(sql, values, (err, result) => {
+      if (err) {
+        console.error('Error executing SQL query:', err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        res.status(200).send('Form submitted successfully');
+      }
+
+      client.end();
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
+
