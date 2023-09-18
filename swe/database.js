c3b94a361c3a8cb7a5f1b2bfd3ed4603c9b0b4ac
Ladi Bada <46547545+RumaboyLotti@users.noreply.github.com>
2023-09-18 00:03:28 -0500

    moving db to postgres

--- swe/database.js
+++ swe/database.js
@@ -1,10 +1,10 @@
-var mysql = require('mysql2');
+var postgres = require('pg');
 
-const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  port: 3306,
-  user: 'root',
-  password: 'Xenophobia',
+const connection = postgres.Connection({
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: 'Xylophobia',
   database: 'Might-Auth',
 });
   
